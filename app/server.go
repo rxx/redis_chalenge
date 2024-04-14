0d1be4792411c1a7ad260a5578ed44e929b79441
Maksym Litvynov <rxx@users.noreply.github.com>
2024-04-14 16:25:26 +0300

    Extract main functions

--- app/server.go
+++ app/server.go
@@ -8,49 +8,23 @@ import (
 	"time"
 )
 
+const (
+  MasterRole = "master"
+  SlaveRole = "slave"
+)
+
 type NodeConfig struct {
+  Host string
 	Port string
 	Role string
+  MasterConfig *NodeConfig
 }
 
 var nodeConfig *NodeConfig
 
-type replicaFlags []string
-
-func (f *replicaFlags) String() string {
-	if len(*f) == 2 {
-		return (*f)[0] + ":" + (*f)[1]
-	}
-
-	return ""
-}
-
-func (f *replicaFlags) Set(value string) error {
-	fmt.Printf("Set %v\n", value)
-	*f = append(*f, value)
-	return nil
-}
-
-var masterConfig struct {
-	host string
-	port string
-}
 
 func main() {
-	port := flag.String("port", "6379", "Redis port [Default: 6379]")
-	masterHost := flag.String("replicaof", "", "master_host")
-	masterPort := flag.String("", "", "port")
-
-	flag.Parse()
-
-	fmt.Printf("OS args %v\n", os.Args)
-	fmt.Printf("Flag args %v\n", flag.Args())
-
-	// OS args [/tmp/tmp.cFFFnP --port 6380 --replicaof localhost 6379]
-	// [your_program] Flag args [6379]
-
-	masterConfig.host = *masterHost
-	masterConfig.port = *masterPort
+	port, masterHost, masterPort := parseArgs()
 
 	nodeConfig = &NodeConfig{Port: *port}
 	role := "master"
@@ -63,7 +37,25 @@ func main() {
 
 	fmt.Printf("NodeConfig %v\n", nodeConfig)
 
-	tcp, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", nodeConfig.Port))
+	startServer()
+}
+
+func parseArgs() (string, string, string) {
+  port := flag.String("port", "6379", "Redis port [Default: 6379]")
+	masterHost := flag.String("replicaof", "", "master_host")
+	tailArgs := flag.Args()
+	flag.Parse()
+
+  var masterPort string
+	if len(tailArgs) > 0 {
+    masterPort = tailArgs[0]
+	}
+  
+  return *port, *masterHost, masterPort
+}
+
+func startServer() {
+  tcp, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", nodeConfig.Port))
 	if err != nil {
 		fmt.Printf("Failed to bind to port %s\n", nodeConfig.Port)
 		os.Exit(1)
